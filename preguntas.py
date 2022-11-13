d0458269fcf17b1c347e243ff9d1fce281ee8e07
aadiosap <aadiosap@unal.edu.co>
2022-11-13 17:56:16 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -90,7 +90,8 @@ def pregunta_03():
     # un vector con valores entre el máximo y el mínimo de X_fertility
     prediction_space = np.linspace(
         X_fertility.min(),
-        X_fertility.max()
+        X_fertility.max(),
+        len(X_fertility)
     ).reshape(-1, 1)
 
     # Entrene el modelo usando X_fertility y y_life
